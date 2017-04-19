08737ca35b2a1aac211fbf433219544cbd766e27
db1996 <dylanbos1996@gmail.com>
2017-04-19 15:31:24 +0200

    added blank files

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+	<meta name="viewport"  content="width=device-width, initial-scale=1.0">
+	<title>Proftaak</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css?v=1">
+</head>
+<body>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
