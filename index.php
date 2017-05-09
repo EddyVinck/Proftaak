d7e143ff4bc847ab152cbdeac7d4d8f7a2330f09
db1996 <dylanbos1996@gmail.com>
2017-05-09 11:22:37 +0200

    emptied the files

--- index.php
+++ index.php
@@ -12,9 +12,26 @@
     </head>
 </head>
 <body>
-	
-    <script type="text/javascript" src="js/main.js"></script>
+  <ul id="slide-out" class="side-nav">
+      <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+      <li><a href="#! " class="waves-effect">Second Link</a></li>
+      <li><div class="divider"></div></li>
+      <li><a class="subheader waves-effect">Subheader</a></li>
+      <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+    </ul>
+  <div style="width:100vw;height:200vh;"></div>
+  <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="material-icons">menu</i></a>
+  <script type="text/javascript" src="js/main.js"></script>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 	<script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
+<script>
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+</script>
 </html>
