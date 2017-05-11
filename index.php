210ddcf136fe3940e569a850cbd23b8222465263
Eddy Vinck <eddyvinck95@gmail.com>
2017-05-11 11:11:27 +0200

    added featured projects section to the homepage and inserted the footer as well.

--- index.php
+++ index.php
@@ -274,32 +274,92 @@ if (isset($_POST['rol'])){
         </div>
       </div>
       <div class="row">
-            <div class="col s4">
-              <div class="center">
-                <i class="material-icons">flash_on</i>
-                <p class="promo-caption">Speeds up development</p>
-                <p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
+        <div class="col s4">
+          <div class="center">
+            <i class="material-icons">flash_on</i>
+            <p class="promo-caption">Speeds up development</p>
+            <p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
+          </div>
+        </div>
+        <div class="col s4">
+          <div class="center">
+            <i class="material-icons">group</i>
+            <p class="promo-caption">User Experience Focused</p>
+            <p class="light center">By utilizing elements and principles of Material Design, we were able to create a framework that focuses on User Experience.</p>
+          </div>
+        </div>
+        <div class="col s4">
+          <div class="center">
+            <i class="material-icons">settings</i>
+            <p class="promo-caption">Easy to work with</p>
+            <p class="light center">We have provided detailed documentation as well as specific code examples to help new users get started.</p>
+          </div>
+        </div>
+      </div>
+      <!-- end of promo row -->
+      <div class="row">
+        <div class="col s12">
+          <h3 class="center-align">Featured Projects</h3>
+          <h5 class="center-align">Bekijk al deze coole dingen die jij niet kan</h5>
+        </div>
+      </div>
+      <div class="row">
+          <div class="col s12 m4 offset-m2">
+            <div class="card">            
+              <div class="card-image">
+                <img src="https://baconmockup.com/999/749/">
+                <span class="card-title">Featured project</span>
+                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
               </div>
-            </div>
-            <div class="col s4">
-              <div class="center">
-                <i class="material-icons">group</i>
-                <p class="promo-caption">User Experience Focused</p>
-                <p class="light center">By utilizing elements and principles of Material Design, we were able to create a framework that focuses on User Experience.</p>
-              </div>
-            </div>
-            <div class="col s4">
-              <div class="center">
-                <i class="material-icons">settings</i>
-                <p class="promo-caption">Easy to work with</p>
-                <p class="light center">We have provided detailed documentation as well as specific code examples to help new users get started.</p>
+              <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
               </div>
             </div>
           </div>
-    </div>  
+          <div class="col s12 m4">
+            <div class="card">
+              <div class="card-image">
+                <img src="https://baconmockup.com/999/749/">
+                <span class="card-title">Featured project</span>
+                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+              </div>
+              <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+              </div>          
+            </div>
+          </div>
+      </div>
+      <!--end of featured projects -->
+    </div>
+    <!--end of info section's container div   -->
   </div>
-  
+  <!--end of info section -->
 </main>
+<footer class="page-footer teal">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+            <h5 class="white-text">Footer Content</h5>
+            <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+            <h5 class="white-text">Links</h5>
+            <ul>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+            </ul>
+            </div>
+        </div>
+        </div>
+        <div class="footer-copyright">
+        <div class="container">
+        &copy 2014 Copyright Text
+        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+        </div>
+    </div>
+</footer>
   
   <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <!--https://code.jquery.com/jquery-3.2.1.js ???-->
@@ -315,7 +375,7 @@ if (isset($_POST['rol'])){
         if (target.length) {
           $('html, body').animate({
             scrollTop: target.offset().top
-          }, 1000);
+          }, 600);
           return false;
         }
       }
