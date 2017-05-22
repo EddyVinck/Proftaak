7ff9734e4d60a0ffc5b5aa96bbf3436902d88684
db1996 <dylanbos1996@gmail.com>
2017-05-22 15:35:53 +0200

    did the same with footers

--- unauthorized.php
+++ unauthorized.php
@@ -20,46 +20,7 @@ if($_SESSION['rol'] == ""){
     </head>
 </head>
 <body >
-<header>    
-    <nav class="top-nav teal">
-        <div class="nav-wrapper">
-            <div class="container">
-        <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-            <div class="col s12" style="padding: 0 .75rem;">                
-                <a href="index.php" class="brand-logo">Logo</a>        
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="projecten_lijst.php?college=<?php echo $_SESSION['college_id'];?>" class=" waves-effect"><i class="small material-icons left">home</i>Mijn College</a></li>
-                <li><a href="colleges.php" class=" waves-effect"><i class="small material-icons left">view_module</i>Colleges</a></li>
-                <li><a href="#inbox.php" class=" waves-effect"><i class="small material-icons left">message</i>Priveberichten</a></li>
-                <li><a href="index.php?logout=true" class=" waves-effect"><i class="small material-icons left">exit_to_app</i> Log uit </a></li>
-            </ul>
-            </div>       
-            <!--<a href="#" class="brand-logo">Logo</a>-->
-            </div>        
-        </div>        
-    </nav>    
-</header>
-<sidenav>
-    <ul id="slide-out" class="side-nav">
-        <li><div class="userView">
-            <div class="background">
-                <img src="images/office.jpg">
-            </div>
-                <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
-                <a href="#!name"><span class="white-text name">John Doe</span></a>
-                <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
-            </div>
-        </li>
-        <li><a href="projecten_lijst.php?college=<?php echo $_SESSION['college_id'];?>"><i class="small material-icons left">home</i>Mijn College</a></li>
-        <li><a href="colleges.php"><i class="small material-icons left">view_module</i>Colleges</a></li>
-        <li><a href="#inbox.php"><i class="small material-icons left">message</i>Priveberichten</a></li>
-        <li><a href="index.php?logout=true"><i class="small material-icons left">exit_to_app</i> Log uit </a></li>
-        <li><a href="#!">Second Link</a></li>
-        <li><div class="divider"></div></li>
-        <li><a class="subheader">Subheader</a></li>
-        <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
-  </ul>
-</sidenav>
+<?php createHeader();?>
 <main class="valign-wrapper">
             <!--Work in progress
     deze pagina is mobile-first ontworpen 
@@ -77,31 +38,7 @@ if($_SESSION['rol'] == ""){
             </div>
     </div>
 </main>
-<footer class="page-footer teal">
-    <div class="container">
-        <div class="row">
-            <div class="col l6 s12">
-            <h5 class="white-text">Footer Content</h5>
-            <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-            </div>
-            <div class="col l4 offset-l2 s12">
-            <h5 class="white-text">Links</h5>
-            <ul>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-            </ul>
-            </div>
-        </div>
-        </div>
-        <div class="footer-copyright">
-        <div class="container">
-        &copy 2014 Copyright Text
-        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-        </div>
-    </div>
-</footer>
+<<?php createFooter();?>
   <script type="text/javascript" src="js/main.js"></script>
   <script type="text/javascript" src="js/ajaxfunctions.js"></script>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
